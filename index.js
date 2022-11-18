38a08bf9396821c96490d48a4c685808da077ebb
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2022-11-17 19:11:09 -0600

    Mostrando ID

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-import {saveTask} from './firebase.js'
-
-window.addEventListener('DOMcontentLoader' , () => {
-})
+import { saveTask, getTasks,onGetTasks } from './firebase.js'
 
 const taskForm = document.getElementById('task-form')
+const tasksContainer = document.getElementById('tasks-container')
 
-taskForm.addEventListener('submit',(e) =>{
+window.addEventListener('DOMContentLoaded', async () => {
+
+    onGetTasks((querySnapshot)=>{
+        let html = ""
+           
+        querySnapshot.forEach(doc => {
+            const task = doc.data()
+           html += `
+           <div>
+                <h3>${task.title}</h3>
+                <p>${task.description}</p>
+                <button class='btn-delete' data-id="${doc.id}">Delete</button>
+           </div>
+           `  
+        })
+        tasksContainer.innerHTML = html
+
+        const btnsDelete = tasksContainer.querySelectorAll('.btn-delete')
+
+        btnsDelete.forEach(btn =>{
+            btn.addEventListener('click', ({target:{dataset}}) =>{
+                console.log(dataset.id)
+            })
+        })
+    })   
+})
+
+taskForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    
-   const title = taskForm['task-title']
-   const description =  taskForm['task-description']
+
+    const title = taskForm['task-title']
+    const description = taskForm['task-description']
 
     saveTask(title.value, description.value)
+
+    taskForm.reset()
+
 })
